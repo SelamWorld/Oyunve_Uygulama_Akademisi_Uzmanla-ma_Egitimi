3ff6bdb5cbfc5f6b48cfe413f2a261de119b1651
halil ibrahim <halil199010@hotmail.com>
2024-01-26 18:21:32 +0300

    character animations,

--- Controller/RuntimeMovement.cs
+++ Controller/RuntimeMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,11 +11,17 @@ public class RuntimeMovement : MonoBehaviour
     private MovementScript _input;
     private CharacterController _controller;
     public float fraction;
+    private Animator _animator;
     // Start is called before the first frame update
+    private void Awake()
+    {
+        Application.targetFrameRate = 100;
+    }
     void Start()
     {
         _controller = GetComponent<CharacterController>();
         _input = GetComponent<MovementScript>();        
+        _animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -25,6 +32,9 @@ public class RuntimeMovement : MonoBehaviour
     }
     public void Move(){
         _controller.Move( new Vector3(_input.MoveVal.x * _input.moveSpeed/fraction, 0, _input.MoveVal.y * _input.moveSpeed/fraction));
+        _animator.SetFloat("Speed", MathF.Abs(_controller.velocity.x)+ Mathf.Abs(_controller.velocity.z));
+        print(_controller.velocity.x +" "+ _controller.velocity.z);
+        
     
     }
 }
